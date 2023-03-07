43f677b0453297a58c14a5f02f951b48b9a6bd9c
YSK_NakamuraRyuta <rnakamura@ysk.co.jp>
2023-03-07 11:01:24 +0900

    ValueObject用のベースクラスを作成

---
+++ CsvConverter.Domain/ValueObjects/ValueObject.cs
@@ -0,0 +1,40 @@
+﻿namespace CsvConverter.Domain.ValueObjects
+{
+    /// <summary>
+    /// オブジェクトをプリミティブ型と同様の値比較が行えるようにするBaseクラス
+    /// </summary>
+    /// <typeparam name="T">オブジェクトが保持する値の型</typeparam>
+    public abstract class ValueObject<T> where T : ValueObject<T>
+    {
+
+        public override bool Equals(object? obj)
+        {
+            T? vo = obj as T;
+            if (vo is null)
+            {
+                return false;
+            }
+            return EqualsCore(vo);
+        }
+
+        public static bool operator ==(ValueObject<T> vo1, ValueObject<T> vo2)
+        {
+            return Equals(vo1, vo2);
+        }
+
+        public static bool operator !=(ValueObject<T> vo1, ValueObject<T> vo2)
+        {
+            return !Equals(vo1, vo2);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetHashCodeCore();
+        }
+
+        protected abstract bool EqualsCore(T vo);
+
+        protected abstract int GetHashCodeCore();
+
+    }
+}
