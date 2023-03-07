81be3cbc3ce4a1ac8692bbe70713162a1d14b7ca
rnakamura-sky <rnakamura1234sky@gmail.com>
2023-03-07 17:59:39 +0900

    ファイルの選択をドラッグ&ドロップで実行できるように実装

---
+++ CsvConverter.WPF/Views/Behaviors/FileDragDropBehavior.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CsvConverter.WPF.Views.Behaviors
+{
+    public class FileDragDropBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewDragOver += DragOverFile;
+            AssociatedObject.Drop += DropFile;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewDragOver -= DragOverFile;
+            AssociatedObject.Drop -= DropFile;
+        }
+
+        private void DragOverFile(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void DropFile(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                var dropFiles = e.Data.GetData(DataFormats.FileDrop) as string[];
+                if (dropFiles is null)
+                {
+                    return;
+                }
+                AssociatedObject.Text = dropFiles[0];
+            }
+        }
+    }
+}
